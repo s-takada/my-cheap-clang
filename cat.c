fb7b67e3010380adf918ccdcba958dfe9a8fa763
s-takada <s-takada@tribalmedia.co.jp>
2016-07-21 19:51:43 +0900

    [EDIT] cat.c
    when nothing args, create stream from stdin.

--- cat.c
+++ cat.c
@@ -1,48 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-static void do_cat(const char *path);
-static void die(const char *s);
+static void do_cat(FILE *f);
 
 int
 main(int argc, char *argv[])
 {
     int i;
-    if (argc < 2) {
-        fprintf(stderr, "%s: file name not given\n", argv[0]);
-        exit(1);
+
+    if (argc == 1) {
+        do_cat(stdin);
     }
-    for (i = 1; i < argc; i++) {
-        do_cat(argv[i]);
+    else {
+        for (i = 1; i < argc; i++) {
+            FILE *f;
+
+            f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                exit(1);
+            }
+            do_cat(f);
+            fclose(f);
+        }
     }
     exit(0);
 }
 
-#define BUFFER_SIZE 2048
-
 static void
-do_cat(const char *path)
+do_cat(FILE *f)
 {
-    int fd;
-    unsigned char buf[BUFFER_SIZE];
-    int n;
+    int c;
 
-    fd = open(path, O_RDONLY);
-    if (fd < 0) die(path);
-    for (;;) {
-        n = read(fd, buf, sizeof buf);
-        if (n < 0) die(path);
-        if (n == 0) break;
-        if (write(STDOUT_FILENO, buf, n) < 0) die(path);
+    while ((c = fgetc(f)) != EOF) {
+        if (putchar(c) < 0) exit(1);
     }
-    if (close(fd) < 0) die(path);
-}
-
-static void
-die(const char *s)
-{
-    perror(s);
-    exit(1);
 }
