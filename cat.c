cf71279c7cd5a0f55f44380e3f71506e8688d086
s-takada <s-takada@tribalmedia.co.jp>
2016-07-24 02:57:31 +0900

    [EDIT] cat.c
    add option -e.
    stream contain '\t' or '\n' characters, append end of character to stream out put.

--- cat.c
+++ cat.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-static void do_cat(FILE *f);
+static void do_cat(FILE *f, int escape);
 
 int
 main(int argc, char *argv[])
 {
-
+    int opt;
+    int escape = 0;
     int i;
 
-    if (argc == 1) {
-        do_cat(stdin);
+    while ((opt = getopt(argc, argv, "e")) != -1) {
+        switch (opt) {
+            case 'e':
+                escape = 1;
+                break;
+            case '?':
+                fprintf(stderr, "Usage: %s [-e] [file...]\n", argv[0]);
+                exit(1);
+        }
+    }
+    argc -= optind;
+    argv += optind;
+    if (argc == 0) {
+        do_cat(stdin, escape);
     }
     else {
-        for (i = 1; i < argc; i++) {
+        for (i = 0; i < argc; i++) {
             FILE *f;
 
             f = fopen(argv[i], "r");
@@ -21,7 +35,7 @@ main(int argc, char *argv[])
                 perror(argv[i]);
                 exit(1);
             }
-            do_cat(f);
+            do_cat(f, escape);
             fclose(f);
         }
     }
@@ -29,21 +43,28 @@ main(int argc, char *argv[])
 }
 
 static void
-do_cat(FILE *f)
+do_cat(FILE *f, int escape)
 {
     int c;
 
-    while ((c = fgetc(f)) != EOF) {
-        switch (c) {
-            case '\t':
-                if (fputs("\\t", stdout) == EOF) exit(1);
-                break;
-            case '\n':
-                if (fputs("$\n", stdout) == EOF) exit(1);
-                break;
-            default:
-                if (putchar(c) < 0) exit(1);
-                break;
+    if (escape) {
+        while ((c = fgetc(f)) != EOF) {
+            switch (c) {
+                case '\t':
+                    if (fputs("\\t", stdout) == EOF) exit(1);
+                    break;
+                case '\n':
+                    if (fputs("$\n", stdout) == EOF) exit(1);
+                    break;
+                default:
+                    if (putchar(c) < 0) exit(1);
+                    break;
+            }
+        }
+    }
+    else {
+        while ((c = fgetc(f)) != EOF) {
+            if (putchar(c) < 0) exit(1);
         }
     }
 }
